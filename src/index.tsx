ca2ad7716ebfbe4abde816be4ad70067d952d101
Theo-projects <uzuntudor@yahoo.com>
2020-09-10 18:00:36 +0300

    v-1.0.0-start

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MainLayout } from 'ui/templates/MainLayout/MainLayout';
+import 'styles/styles.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MainLayout />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
