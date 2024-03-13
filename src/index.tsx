17054cd1d287281e92b0b28bbcbb4f3647e83255
CatWeb <uzuntudor@yahoo.com>
2024-03-13 13:09:32 +0200

    some changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { CreateRouter } from 'estafette-router';
 import { ProviderContext } from 'contexts/PizzaContext';
 import { routes } from 'routes';
 
 import 'styles/styles.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ProviderContext>
-      <CreateRouter routes={routes} />
-    </ProviderContext>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <ProviderContext>
+    <CreateRouter routes={routes} />
+  </ProviderContext>
 );
